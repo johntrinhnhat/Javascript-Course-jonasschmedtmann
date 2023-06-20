8bc9bf851bd781852300410cdf3aafb1cbc212ac
Khoi Trinh Nhat <116620830+KHOITRINH03@users.noreply.github.com>
2023-06-20 15:52:02 -0400

    Finish Challenge 3

--- Fundamental Part 1/index.js
+++ Fundamental Part 1/index.js
@@ -44,3 +44,66 @@ if (bmiMark > bmiJohn) {
   console.log(`John's BMI (${bmiJohn}) is higher than Mark's (${bmi})`);
 } else {
   console.log(`Mark's BMI (${bmiMark}) is equal to John's BMI (${bmiJohn})`);
+  
+// Coding Challenge #3
+
+/*
+There are two gymnastics teams, Dolphins and Koalas. They compete against each other 3 times. The winner with the highest average score wins the a trophy!
+
+1. Calculate the average score for each team, using the test data below
+2. Compare the team's average scores to determine the winner of the competition, and print it to the console. Don't forget that there can be a draw, so test for that as well (draw means they have the same average score).
+
+3. BONUS 1: Include a requirement for a minimum score of 100. With this rule, a team only wins if it has a higher score than the other team, and the same time a score of at least 100 points. HINT: Use a logical operator to test for minimum score, as well as multiple else-if blocks 😉
+4. BONUS 2: Minimum score also applies to a draw! So a draw only happens when both teams have the same score and both have a score greater or equal 100 points. Otherwise, no team wins the trophy.
+
+TEST DATA: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+TEST DATA BONUS 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+TEST DATA BONUS 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+
+GOOD LUCK 😀
+*/
+
+  // const dolphinsScore = ((96 + 108 + 89) / 3).toFixed(2);
+  // const koalasScore = ((88 + 91 + 110) / 3).toFixed(2);
+  // console.log(dolphinsScore, koalasScore);
+
+  // if (dolphinsScore < koalasScore) {
+  //   console.log(`Koaslas Team won the trophy 🏆`);
+  // } else if (dolphinsScore > koalasScore) {
+  //   console.log(`Dolphins Team won the trophy 🏆`);
+  // } else {
+  //   console.log(`Both team have the same average score 👏`);
+  // }
+
+  //BONUS 1:
+
+  // const dolphinsScore = ((97 + 112 + 101) / 3).toFixed(2);
+  // const koalasScore = ((109 + 95 + 123) / 3).toFixed(2);
+  // console.log(dolphinsScore, koalasScore);
+  // if (dolphinsScore < koalasScore && koalasScore >= 100) {
+  //   console.log(`Koaslas Team won the trophy 🏆`);
+  // } else if (dolphinsScore > koalasScore && dolphinsScore >= 100) {
+  //   console.log(`Dolphins Team won the trophy 🏆`);
+  // } else {
+  //   console.log(`No team won the trophy 🏆`);
+  // }
+
+  //BONUS 2:
+  const dolphinsScore = ((97 + 112 + 101) / 3).toFixed(2);
+  const koalasScore = ((109 + 95 + 106) / 3).toFixed(2);
+  console.log(dolphinsScore, koalasScore);
+
+  if (dolphinsScore < koalasScore && koalasScore >= 100) {
+    console.log(`Koaslas Team won the trophy 🏆`);
+  } else if (dolphinsScore > koalasScore && dolphinsScore >= 100) {
+    console.log(`Dolphins Team won the trophy 🏆`);
+  } else if (
+    dolphinsScore === koalasScore &&
+    koalasScore >= 100 &&
+    dolphinsScore >= 100
+  ) {
+    console.log(`Both team won the trophy 🏆`);
+  } else {
+    console.log(`No team won the trophy 😢`);
+  }
+}
