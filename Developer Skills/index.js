8cb682ecad37ee5955b599fd6bfdc76c6005593a
Khoi Trinh <116620830+KHOITRINH03@users.noreply.github.com>
2023-06-21 13:36:53 -0400

    Solved Problem 1

--- Developer Skills/index.js
+++ Developer Skills/index.js
@@ -1,1 +1,22 @@
+// PROBLEM 1:
+// We work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate the temperature amplitude.
+// Keep in mind that sometimes there might be a sensor error."
 
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+const calcTempAmplitude = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+const amplitude = calcTempAmplitude(temperatures);
+console.log(amplitude);
